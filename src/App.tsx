cda557b9381047bc366237648fdd960ff100ca5c
konovalM <konovalenko.ii4elka@mail.ru>
2023-02-04 01:24:37 +0300

    [feat] classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-
-    console.log(theme)
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Сменить тему</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
