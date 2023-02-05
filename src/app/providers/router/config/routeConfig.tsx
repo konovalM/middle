413ca28ee9b3b2d3cc97df9128fde66a9f9e1f66
konovalM <konovalenko.ii4elka@mail.ru>
2023-02-05 19:58:23 +0300

    [feat] refactoring to feature sliced design architecture

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -1,24 +1,15 @@
 import {RouteProps} from "react-router-dom";
 import {MainPage} from "pages/MainPage";
 import {AboutPage} from "pages/AboutPage";
-
-export enum AppRoutes {
-    MAIN = 'main',
-    ABOUT = 'about'
-}
-
-export const RoutePath: Record<AppRoutes, string> = {
-    [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: '/about'
-}
+import {AppRoutes, RoutePath} from "shared/consts/routeConsts/routeConsts";
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
-        path: RoutePath.main,
+        path: RoutePath[AppRoutes.MAIN],
         element: <MainPage />
     },
     [AppRoutes.ABOUT]: {
-        path: RoutePath.about,
+        path: RoutePath[AppRoutes.ABOUT],
         element: <AboutPage />
     }
 }
