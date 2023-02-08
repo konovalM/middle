a4550ca9f2f6a0845ff92e1f2333f3f8553dd31d
konovalM <konovalenko.ii4elka@mail.ru>
2023-02-08 23:00:15 +0300

    [feat] added plugins for updating browser withot reload

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,9 +2,10 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[]{
-    return [
+    const plugins =  [
         new HTMLWebpackPlugin({
             template: paths.html
         }),
@@ -17,6 +18,12 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: isDev,
-        })
+        }),
     ]
+    if (isDev){
+        // это взаимозаменяемые плагины для обновления кода без перезагрузки страницы в браузере
+        plugins.push(new ReactRefreshPlugin())
+        plugins.push(new webpack.HotModuleReplacementPlugin)
+    }
+    return plugins
 }
