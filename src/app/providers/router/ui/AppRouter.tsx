2f74f742ab6a21edce1b35564a94ccffc3e8111f
konovalM <konovalenko.ii4elka@mail.ru>
2023-02-12 01:45:54 +0300

    [feat] page loader & not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,22 +1,27 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'widgets/PageLoader';
 import { routeConfig } from '../config/routeConfig';
 
-const AppRouter = () => {
-    const { t } = useTranslation();
-    return (
-        <Suspense fallback={t('Загрузка')}>
-            <Routes>
-                {
-                    Object.values(routeConfig)
-                        .map(({ path, element }) => (
-                            <Route path={path} element={<div className="page-wrapper">{element}</div>} key={path} />
-                        ))
-                }
-            </Routes>
-        </Suspense>
-    );
-};
+const AppRouter = () => (
+    <Suspense fallback={<PageLoader />}>
+        <Routes>
+            {
+                Object.values(routeConfig)
+                    .map(({ path, element }) => (
+                        <Route
+                            path={path}
+                            element={(
+                                <div className="page-wrapper">
+                                    {element}
+                                </div>
+                            )}
+                            key={path}
+                        />
+                    ))
+            }
+        </Routes>
+    </Suspense>
+);
 
 export default AppRouter;
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loader.scss';
+
+const Loader = () => (
+    <div className="lds-heart">
+        <div />
+    </div>
+);
+
+export default Loader;
