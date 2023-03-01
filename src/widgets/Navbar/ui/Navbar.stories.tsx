dff74c05ca0b2c937efca5f4dd6ccfb1f8cdb0be
konovalM <konovalenko.ii4elka@mail.ru>
2023-03-02 00:58:36 +0300

    [fix] storybook navbar store decorators fixes, add new cases

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import 'app/styles/index.scss';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Themes } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { Navbar } from './Navbar';
 
 export default {
@@ -11,6 +12,7 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [StoreDecorator(({ user: undefined }))],
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
@@ -21,3 +23,11 @@ Light.args = {};
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators = [ThemeDecorator(Themes.DARK)];
+
+export const LightLoggedIn = Template.bind({});
+LightLoggedIn.args = {};
+LightLoggedIn.decorators = [ThemeDecorator(Themes.DARK), StoreDecorator({ user: { authData: { id: '123', name: 'ii4elka' } } })];
+
+export const DarkLoggedIn = Template.bind({});
+DarkLoggedIn.args = {};
+DarkLoggedIn.decorators = [ThemeDecorator(Themes.DARK), StoreDecorator({ user: { authData: { id: '123', name: 'ii4elka' } } })];
